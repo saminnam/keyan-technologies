6b9b7dabc7901260323397a71a5ca8b0e4490c9e
saminnam <innam8264@gmail.com>
2024-12-28 10:45:56 +0530

    Update Logos.jsx

--- src/Components/Logos.jsx
+++ src/Components/Logos.jsx
@@ -14,35 +14,35 @@ const logos = [
 ];
 
 const Logos = () => {
-    const [currentLogoIndex, setCurrentLogoIndex] = useState(0);
+  const [currentLogoIndex, setCurrentLogoIndex] = useState(0);
 
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setCurrentLogoIndex((prevIndex) =>
-          prevIndex === logos.length - 1 ? 0 : prevIndex + 1
-        );
-      }, 1000);
-      return () => clearInterval(interval);
-    }, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentLogoIndex((prevIndex) =>
+        prevIndex === logos.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <ul className="mt-16 grid grid-cols-1 sm:mt-24 sm:grid-cols-3 sm:gap-12 md:grid-cols-5 md:gap-4">
-          {logos &&
-            logos.map((logoItem, index) => (
-              <li
-                key={index}
-                className={`flex justify-center transition-opacity duration-1000 z-40 ${
-                  index === currentLogoIndex ? "opacity-100" : "opacity-50"
-                }`}
-              >
-                <img
-                  src={logoItem.logo}
-                  alt={`Logo ${logoItem.alt}`}
-                  className="inline-block h-9"
-                />
-              </li>
-            ))}
-        </ul>
-  )
-}
+      {logos &&
+        logos.map((logoItem, index) => (
+          <li
+            key={index}
+            className={`flex justify-center transition-opacity duration-1000 z-40 ${
+              index === currentLogoIndex ? "opacity-100" : "opacity-50"
+            }`}
+          >
+            <img
+              src={logoItem.logo}
+              alt={`Logo ${logoItem.alt}`}
+              className="inline-block h-9"
+            />
+          </li>
+        ))}
+    </ul>
+  );
+};
 
-export default Logos
+export default Logos;
